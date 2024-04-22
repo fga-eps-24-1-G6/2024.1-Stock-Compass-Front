fcdf4ec4b4871caef2c6e43eb29fdb8f9c50b0af
vital14 <limavictor49@gmail.com>
2024-04-22 18:06:56 -0300

    adding clerk buttons to provider

--- lib/app.tsx
+++ lib/app.tsx
@@ -1,9 +1,23 @@
 import "@/styles/globals.css";
-import { ClerkProvider } from "@clerk/nextjs/server";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 import type { AppProps } from "next/app";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ClerkProvider {...pageProps}>
+      <header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
       <Component {...pageProps} />
     </ClerkProvider>
   );

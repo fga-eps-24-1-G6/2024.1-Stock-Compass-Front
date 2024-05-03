98eb2acc6d64acdfa26548d969ff63d213c5874c
jvBatista <jvtexbat@hotmail.com>
2024-05-03 13:06:47 -0300

    fix: sonar issues

--- app/stocks/[ticker]/IndicatorsSummary/IndicatorsSummary.tsx
+++ app/stocks/[ticker]/IndicatorsSummary/IndicatorsSummary.tsx
@@ -3,78 +3,24 @@ import { Card } from "../../../../components/ui/card";
 
 async function getIndicatorsSummary(ticker: string) {
   return [
-    {
-      indicator: "P/L",
-      value: "3.0",
-    },
-    {
-      indicator: "P/VP",
-      value: "0.95",
-    },
-    {
-      indicator: "DIV YIELD",
-      value: "5.0%",
-    },
-    {
-      indicator: "PAYOUT",
-      value: "65.0%",
-    },
-    {
-      indicator: "MARGEM LÍQ",
-      value: "15.0%",
-    },
-    {
-      indicator: "MARGEM BRUTA",
-      value: "25.0%",
-    },
-    {
-      indicator: "MARGEM EBIT",
-      value: "5.0%",
-    },
-    {
-      indicator: "EV/EBIT",
-      value: "5.0",
-    },
-    {
-      indicator: "EV/EBITDA",
-      value: "5.1",
-    },
-    {
-      indicator: "VPA",
-      value: "4.0",
-    },
-    {
-      indicator: "LPA",
-      value: "3.0",
-    },
-    {
-      indicator: "ROE",
-      value: "14.0%",
-    },
-    {
-      indicator: "ROIC",
-      value: "16.0%",
-    },
-    {
-      indicator: "ROA",
-      value: "13.0%",
-    },
-    {
-      indicator: "CAGR LUCRO",
-      value: "15.5%",
-    },
-    {
-      indicator: "CAGR REC",
-      value: "10.0%",
-    },
-    {
-      indicator: "DÍV LÍQ/PAT LÍQ",
-      value: "1.5",
-    },
-    {
-      indicator: "DÍV LÍQ/EBIT",
-      value: "2.0",
-    },
+    { indicator: "P/L", value: "3.0", },
+    { indicator: "P/VP", value: "0.95", },
+    { indicator: "DIV YIELD", value: "5.0%", },
+    { indicator: "PAYOUT", value: "65.0%", },
+    { indicator: "MARGEM LÍQ", value: "15.0%", },
+    { indicator: "MARGEM BRUTA", value: "25.0%", },
+    { indicator: "MARGEM EBIT", value: "5.0%", },
+    { indicator: "EV/EBIT", value: "5.0", },
+    { indicator: "EV/EBITDA", value: "5.1", },
+    { indicator: "VPA", value: "4.0", },
+    { indicator: "LPA", value: "3.0", },
+    { indicator: "ROE", value: "14.0%", },
+    { indicator: "ROIC", value: "16.0%", },
+    { indicator: "ROA", value: "13.0%", },
+    { indicator: "CAGR LUCRO", value: "15.5%", },
+    { indicator: "CAGR REC", value: "10.0%", },
+    { indicator: "DÍV LÍQ/PAT LÍQ", value: "1.5", },
+    { indicator: "DÍV LÍQ/EBIT", value: "2.0", },
   ];
 }
 

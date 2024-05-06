fe6ea3f04339e26f34876d3fdd4457145d5262d0
vital14 <limavictor49@gmail.com>
2024-05-06 19:10:08 -0300

    fix: removing children from props

--- app/stockssearch/[ticker]/page.tsx
+++ app/stockssearch/[ticker]/page.tsx
@@ -1,47 +1,44 @@
-import SingleColumn from "@/components/templates/SingleColumn"
+import SingleColumn from "@/components/templates/SingleColumn";
 import { Button } from "@/components/ui/button";
-import {
-    Card,
-    CardContent,
-    CardHeader,
-} from "@/components/ui/card"
-import {Input} from "@/components/ui/input";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 import ReturnedStocks from "./ReturnedStocks/ReturnedStocks";
 
-
-
-function SectionCard({ children, id }: { children: React.ReactNode, id?: string }) {
-    return (
-        <Card className="w-full rounded-3xl" id={id}>
-            {children}
-        </Card>
-    )
-};
-
+function SectionCard({
+  children,
+  id,
+}: {
+  children: React.ReactNode;
+  id?: string;
+}) {
+  return (
+    <Card className="w-full rounded-3xl" id={id}>
+      {children}
+    </Card>
+  );
+}
 
 export default async function StockSearchPage({ params }: any) {
+  return (
+    <SingleColumn>
+      <SectionCard>
+        <CardHeader>
+          <p>Pesquisar Ações</p>
+        </CardHeader>
 
-    return (
-        <SingleColumn>
-            <SectionCard>
-                <CardHeader>
-                    <p>Pesquisar Ações</p>
-                </CardHeader>
-
-                <CardContent className="flex flex-col gap-4 md:gap-6 justify-between">
-                    <div className="flex flex-row w-full gap-5">
-                    <Input type="text" placeholder="Nome da ação/empresa"/>
-                    <Button variant={"outline"} children={"Filtros"}></Button>
-                    </div>
-                    <div>
-                    <Button children={"Pesquisar"}></Button>
-                    </div>
-                </CardContent>
-            </SectionCard>
-            <div className="grid grid-cols-2">
-                <ReturnedStocks ticker={""}></ReturnedStocks>
-            </div>
-        </SingleColumn>
-
-    )
+        <CardContent className="flex flex-col gap-4 md:gap-6 justify-between">
+          <div className="flex flex-row w-full gap-5">
+            <Input type="text" placeholder="Nome da ação/empresa" />
+            <Button variant={"outline"}>Filtros</Button>
+          </div>
+          <div>
+            <Button>Pesquisar</Button>
+          </div>
+        </CardContent>
+      </SectionCard>
+      <div className="grid grid-cols-2">
+        <ReturnedStocks ticker={""}></ReturnedStocks>
+      </div>
+    </SingleColumn>
+  );
 }
